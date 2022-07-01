0f1d968dca63a4e213fe5148f1ead6f951d42a42
Mohammad Aqdas <95559517+aqdas77@users.noreply.github.com>
2022-07-02 01:41:05 +0530

    update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const socket = require("socket.io")
 const app = express();                      //initialized and server ready
 app.use(express.static("public"));          //public and app.js both are at same level
 
-let port = 5000;
+let port = process.env.PORT || 5000;
 let server = app.listen(port,()=>{
  console.log("server is listening on port 5000")
 })
